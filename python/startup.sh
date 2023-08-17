78286629832c80976f55bdbe0c771211f905c43c
Eduardo V. Argenton <eduardov.argenton@outlook.com>
2023-08-17 17:04:58 -0300

    Correções gerais

--- python/startup.sh
+++ python/startup.sh
@@ -3,6 +3,7 @@ sleep 7
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --name=$DJANGO_SUPERUSER_NAME
+python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL
 
-gunicorn lpn.wsgi:application -b 0.0.0.0:8000
+#gunicorn monitor_deslizamento_solo.wsgi:application -b 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
