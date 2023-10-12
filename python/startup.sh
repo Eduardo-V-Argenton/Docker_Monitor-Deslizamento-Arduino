8712a7185a6acd715141e04c46e8a0ea02cb6027
Eduardo V. Argenton <eduardov.argenton@outlook.com>
2023-10-12 20:17:53 -0300

    Remoção nginx + gunicorn

--- python/startup.sh
+++ python/startup.sh
@@ -5,5 +5,5 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL
 
-#gunicorn monitor_deslizamento_solo.wsgi:application -b 0.0.0.0:8000
-python manage.py runserver 0.0.0.0:8000
+gunicorn monitor_deslizamento_solo.wsgi:application -b 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
